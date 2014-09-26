a54ebb99151d84b03fad3ff5d3eea48a24d8ac4a
alvaro <alvarogonzalezsotillo@gmail.com>
2014-09-26 12:09:24 +0200

    Initial implementation of Board, doesn't work yet

--- src/main/scala/dideco/Board.scala
+++ src/main/scala/dideco/Board.scala
@@ -16,6 +16,9 @@ trait Board[T] {
   val rows: Int
 
   def pieceAt(column: Int, row: Int): T
+  def pieceAt( l : Location ) : T = pieceAt( l.col, l.row )
+  def free( l: Location ) : Boolean = free( l.col, l.row )
+  def free(column: Int, row: Int ) : Boolean = pieceAt(column,row) == null
 
   def locationOf(piece: T): Location
 
@@ -41,7 +44,7 @@ trait Board[T] {
   override lazy val hashCode = toString.hashCode
 }
 
-object Board{
+object Board extends LazyLogging{
 
 
   trait BFSBoardDefinition[T] extends BFS.BFSDefinition[Board[T]]{
@@ -56,6 +59,46 @@ object Board{
     }
 
     BFS(board,bfsDef)
+  }
+
+  def apply( width: Int, height: Int, pieces : Seq[OrientablePiece] ) : Board[OrientablePiece] = {
+    assert( pieces.size == width * height )
+
+
+    new Board[OrientablePiece]{
+      override val columns = width
+      override val rows = height
+
+      def locationFromIndex( i: Int ) = Location(i%columns,i/rows) ensuring ( i >= 0 && i < columns*rows )
+      def indexFromLocation( column: Int, row: Int ) : Int = (column + row*rows) ensuring inside(Location(column,row))
+
+      def indexFromLocation( l: Location ) : Int = indexFromLocation(l.col, l.row )
+
+      override def pieceAt(column: Int, row: Int): OrientablePiece = pieces( indexFromLocation(column, row) )
+
+
+
+      override def locationOf(piece: OrientablePiece): Location = pieces.indexOf(piece) match{
+        case -1 =>  null
+        case i  => locationFromIndex(i)
+      }
+
+      override val oneMovementBoards: Seq[Board[OrientablePiece]] = {
+        val turns = Turn.values.toArray
+        for( c <- 0 until columns ;
+             r <- 0 until rows if( free(c,r) ) ;
+             location = Location(c,r) ;
+             piece = pieceAt(location) if ( !free(location) ) ;
+             t  <- turns ;
+             newLocation = location.to(t) if( inside(newLocation) && free(newLocation) ) ;
+             newPiece = piece.turn(if (Turn.horizontal(t)) t else Turn.opposite(t) ) ) yield{
+          val newPieces = pieces.toArray.clone
+          newPieces( indexFromLocation(c,r) ) = null
+          newPieces( indexFromLocation(newLocation) ) = newPiece
+          apply( width, height, newPieces)
+        }
+      }
+    }
   }
 
 }
---
+++ src/test/scala/dideco/BoardTest.scala
@@ -0,0 +1,27 @@
+package dideco
+
+/**
+ * Created by alvaro on 26/09/2014.
+ */
+import Orientation.Orientation
+import Turn.Turn
+import org.junit.runner.RunWith
+import org.scalatest.FlatSpec
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class BoardTest extends FlatSpec {
+
+
+  "The two implementations of one piece board" should "give the same positions in a 2x2 board with one piece" in{
+    val piece = OrientablePiece.from( OrientablePiece.originalOrientable )
+
+    val board1 = OnePieceBoard(2,2,piece,Location(0,0))
+    val board2 = Board(2,2,Seq(piece,null,null,null) )
+
+    val b1 = board1.oneMovementBoards.toSet
+    val b2 = board2.oneMovementBoards.toSet
+
+    assert( b1 == b2 )
+  }
+}
